e51331a036f97649617877d72193a19d3dea633e
alexsandrov16 <al3x5.dev@gmail.com>
2023-11-29 22:01:38 -0500

    Add cmmand

--- openbox.sh
+++ openbox.sh
@@ -1,3 +1,64 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
+#/data/data/com.termux/files/usr/bin/bash
 
-echo "Welcome to OpenBox"
+# Banner
+banner() {
+    echo 🧑‍💻 OpenBox 🧑‍💻
+    echo "[1] 🚧 Apache/PHP (NO)"
+    echo "[2] 🐍 Python"
+    echo "[3] 🦀 Rust"
+    echo "[4] 🎨 Zsh/Oh-My-Zsh"
+    echo "[5] 🕵️‍♂️ Nmap"
+}
+
+# Mensaje de instalacion
+mnsg() {
+    echo Iniciando proceso de instalación...
+    sleep 2
+}
+
+# Apache PHP
+web() {
+    echo 😵‍💫 No implementado...
+    #pkg install php-apache
+}
+
+# zsh
+ohZSH() {
+    mnsg
+    pkg install zsh
+}
+
+main() {
+    echo $1
+
+    case $1 in
+    1)
+        web
+        ;;
+    2)
+        # Instalando python
+        mnsg
+        pkg install python
+        ;;
+    3)
+        # Instalando rust
+        mnsg
+        pkg install rustup
+        ;;
+    4)
+        mnsg
+        ohZSH
+        ;;
+    5)
+        # Instalando nmap
+        mnsg
+        pkg install nmap
+        ;;
+    *)
+        banner
+        ;;
+    esac
+}
+
+main $1
